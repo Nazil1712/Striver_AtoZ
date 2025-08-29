f1524beb8beaf8793779bf5910c4b20e6f7d9305
Nazil1712 <ndhalwala@gmail.com>
2025-08-29 15:36:33 +0530

    Floor and Ceil

---
+++ 4_Step_4/BS_1D_Arrays/5_Floor_And_Ceil.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> getFloorAndCeil(vector<int> arr, int x) {
+
+    int low = 0, n = arr.size(), high = n-1;
+    vector<int> ans(2,-1);
+
+
+    // Code For Floor
+    while(low <= high) {
+        int mid = (low + high) / 2;
+
+        if(arr[mid] <= x) {
+            ans[0] = arr[mid];
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+
+
+
+    // Code for Ceil
+    low = 0, high = n-1;
+    while(low <= high) {
+        int mid = (low + high) / 2;
+
+        if(arr[mid] >= x) {
+            ans[1] = arr[mid];
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+
+int main()
+{
+
+    vector<int> arr = {3, 4, 4, 7, 8, 10};
+    int x = 5;
+
+    vector<int> ans = getFloorAndCeil(arr,x);
+
+    cout << "Floor ==> " << ans[0] << endl;
+    cout << "Ceil  ==> " << ans[1] << endl;
+
+    return 0;
+}
